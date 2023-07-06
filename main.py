430cd972f83b9948129d8d4d2983ac164992d44e
Denis Ustinow <ml_engineer@icloud.com>
2023-07-06 12:28:40 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+from envis import Envis
+
+if __name__ == '__main__':
+
+    # Create an instance of the Envis class
+    env = Envis()
+
+    # Create environment variables
+    env.create_env_variable('MY_STRING', str='Hello, World!')
+    env.create_env_variable('MY_INTEGER', int=42)
+    env.create_env_variable('MY_FLOAT', float=3.14)
+    env.create_env_variable('MY_BOOL', bool=True)
+    env.create_env_variable('MY_DICT', dict={'key': 'value'})
+    env.create_env_variable('MY_INCREMENT', i=1)
+
+    # Retrieve environment variables
+    my_string = env.get_env_variable('MY_STRING')
+    my_integer = env.get_env_variable('MY_INTEGER')
+    my_float = env.get_env_variable('MY_FLOAT')
+    my_bool = env.get_env_variable('MY_BOOL')
+    my_dict = env.get_env_variable('MY_DICT')
+    my_increment = env.get_env_variable('MY_INCREMENT')
+
+    print(my_string)  # Output: Hello, World!
+    print(my_integer)  # Output: 42
+    print(my_float)  # Output: 3.14
+    print(my_bool)  # Output: True
+    print(my_dict)  # Output: {'key': 'value'}
+    print(my_increment)  # Output: 1
